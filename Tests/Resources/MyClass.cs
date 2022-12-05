9b796051de5cd619cd29fd99ae4878b7820198f4
KaShTaN27 <dkachan.lida@mail.ru>
2022-12-05 23:37:11 +0300

    Add resources for tests

---
+++ Tests/Resources/MyClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Jora {
+
+    namespace Jira {
+        
+        public class MyClass {
+
+            public void FirstMethod() {
+                Console.WriteLine("First method");
+            }
+
+            public void SecondMethod() {
+                Console.WriteLine("Second method");
+            }
+
+            public void ThirdMethod(int a) {
+                Console.WriteLine("Third method (int)");
+            }
+
+            public void ThirdMethod(double a) {
+                Console.WriteLine("Third method (double)");
+            }
+
+            private void ThirdMethod(string a) {
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
